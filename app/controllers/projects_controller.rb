58a169239298b836e3006e79ee2c1e34f0fee357
Harold Torres Marino <haroldtm55@gmail.com>
2021-11-03 22:09:43 +1100

    Show and Edit actions handle what users can do depending on their role. Projects index is more explicit. New and Edit forms were refactored

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  # Only users that are Admin or Project Manager are able to see the new project view.
   def new
     if current_user.role_name != "Admin" && current_user.role_name != "Project Manager"
       redirect_to user_path(current_user)
@@ -9,7 +10,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    # binding.pry
+    # If user tries to modify its id in the inspect tool they'll see an error message, otherwise project will be created
     if params[:project][:project_manager_id] == current_user.id.to_s
       project = Project.create(project_params)
       redirect_to user_project_path(current_user, project)
@@ -38,22 +39,39 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    # If user isn't an Admin and tries to see other users projects, they redirect to user's profile
     if current_user.role_name != "Admin" && current_user.id != params[:user_id].to_i
       redirect_to user_path(current_user)
     else
-      @project = Project.find(params[:id])
+      # If user is a Project manager or Lead Dev, they'll be able to see only their projects.
+      if (current_user.role_name == "Project Manager" && current_user.sent_projects.any?{|p| p.id == params[:id].to_i}) ||
+        (current_user.role_name == "Lead Developer" && current_user.received_projects.any?{|p| p.id == params[:id].to_i})
+        @project = Project.find(params[:id])
+      else
+        redirect_to user_path(current_user)
+      end
     end
   end
 
   def edit
-    # binding.pry
-    @project = Project.find(params[:id])
-    @lead_developers = User.lead_developers
+    # If user isn't an Admin and tries to see other users projects, they redirect to user's profile
+    if current_user.role_name != "Admin" && current_user.id != params[:user_id].to_i
+      redirect_to user_path(current_user)
+    else
+    # If user is a Project manager or Lead Dev, they'll be able to see only their projects.
+      if (current_user.role_name == "Project Manager" && current_user.sent_projects.any?{|p| p.id == params[:id].to_i}) ||
+        (current_user.role_name == "Lead Developer" && current_user.received_projects.any?{|p| p.id == params[:id].to_i})
+        @project = Project.find(params[:id])
+        @lead_developers = User.lead_developers
+      else
+        redirect_to user_path(current_user)
+      end
+    end    
   end
 
   def update
-    binding.pry
-    if params[:project][:project_manager_id] == current_user.id.to_s
+  # If user tries to modify its id in the inspect tool they'll see an error message, otherwise project will be updated
+    if params[:project][:project_manager_id].to_i == current_user.id
       project = Project.find(params[:id])
       project.update(project_params)
       redirect_to user_project_path(current_user, project)
