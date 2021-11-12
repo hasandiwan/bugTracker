bab952667633089aeb4a0398e0e0e84375a9497c
Harold Torres Marino <haroldtm55@gmail.com>
2021-11-12 22:43:54 +1100

    Built a demo account creation feature for demonstration purposes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,31 @@
 class WelcomeController < ApplicationController
   skip_before_action :verified_user
   def home
+  end
 
+  def guest_login
+    @guest = User.new
+    @roles = Role.all
+  end
+
+  def create
+    binding.pry
+    guest = User.create(guest_params)
+    session[:user_id] = guest.id
+    redirect_to user_path(guest)
+  end
+
+  private
+  
+  def guest_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :role_id,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#home"
+  get "/guest-login", to: "welcome#guest_login"
+  resources :welcome, only: [:create]
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
